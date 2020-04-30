593ef98d985e21fcf762c0834e7d43dbdd29534a
Michael Sotir <sotirm@umich.edu>
2020-04-30 02:26:23 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,8 +1,8 @@
 def find_element_index(array, value_to_find)
   counter = 0 
   while counter < array.length do 
-    if array[counter] == value_to_find
-      new_val = array.index()
+    
       
+    counter = counter + 1
   end
 end
